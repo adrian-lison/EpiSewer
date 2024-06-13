26d12d746837741b4f5250b67719b69d40d14b3a
Adrian Lison <adrian.lison@bsse.ethz.ch>
2024-06-13 14:30:19 +0200

    Update Influenza example data

--- data-raw/Influenza Zurich.R
+++ data-raw/Influenza Zurich.R
@@ -5,7 +5,7 @@ WW_data <- read_local_data_EAWAG()
 WW_data <- WW_data[wwtp_name == "ARA Werdhoelzli" & target == "IAV-M", ]
 WW_data[,concentration := gc_per_lww / 1000]
 WW_data[,flow := flow * 1000 * 1000]
-WW_data <- WW_data[!is.na(concentration), c("date", "concentration", "flow")]
+WW_data <- WW_data[!is.na(concentration), c("date", "concentration", "replicate_id", "dilution", "total_droplets", "flow")]
 
 WW_data <- WW_data[
   date >= as.Date("2022-09-15") & date < as.Date("2023-05-01"),
@@ -18,8 +18,12 @@ WW_data <- WW_data[
 ]
 data.table::setnafill(WW_data, type = "locf", cols = "flow")
 
+# interpolate total droplets and dilution
+WW_data[is.na(dilution) & !is.na(concentration), dilution := median(WW_data$dilution, na.rm = T)]
+WW_data[is.na(total_droplets) & !is.na(concentration), total_droplets := median(WW_data$total_droplets, na.rm = T)]
+
 Influenza_A_Zurich <- list(
-  measurements = WW_data[, c("date", "concentration")],
+  measurements = WW_data[, c("date", "concentration", "replicate_id", "dilution", "total_droplets")],
   flows = WW_data[, c("date", "flow")],
   units = list(concentration = "gc/mL", flow = "mL/day")
 )
