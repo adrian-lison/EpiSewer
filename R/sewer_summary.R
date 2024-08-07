0442b83c225d63fd94dd4c46c8d17971eb4c079d
Adrian Lison <adrian.lison@bsse.ethz.ch>
2024-08-07 15:46:17 +0200

    Make CrIs customizable in sewer summary

--- R/sewer_summary.R
+++ R/sewer_summary.R
@@ -39,6 +39,8 @@ get_iota_trajectories <- function(fit, T_shift, .metainfo, ndraws = 10) {
 #' @param fit The fitted `EpiSewer` model.
 #' @param data The model data that the `EpiSewer` model was fitted with.
 #' @param .metainfo Meta information about the model data.
+#' @param intervals The credible intervals (CrIs) that should be calculated. By
+#'   default, these are the 50% and 95% CrIs.
 #' @param ndraws Number of exemplary posterior samples that should be extracted.
 #'   (The summaries always use all draws.)
 #'
@@ -62,7 +64,7 @@ get_iota_trajectories <- function(fit, T_shift, .metainfo, ndraws = 10) {
 #' - concentration (posterior summary)
 #' - sample_effects (posterior summary)
 #' @export
-summarize_fit <- function(fit, data, .metainfo, ndraws = 50) {
+summarize_fit <- function(fit, data, .metainfo, intervals = c(0.5, 0.95), ndraws = 50) {
   summary <- list()
   T_shift_R <- with(data, L + S + D - G)
   T_shift_latent <- with(data, L + S + D)
@@ -72,7 +74,9 @@ summarize_fit <- function(fit, data, .metainfo, ndraws = 50) {
   summary[["R"]] <- get_summary_1d_date(
     fit, "R",
     T_shift = T_shift_R, .metainfo = .metainfo,
-    intervals = c(0.5, 0.95)
+    var_forecast = "R_forecast",
+    h_forecast = .metainfo$forecast_horizon,
+    intervals = intervals
   )
   summary[["R"]]$seeding <- FALSE
   summary[["R"]][1:(data$G), "seeding"] <- TRUE
@@ -87,7 +91,9 @@ summarize_fit <- function(fit, data, .metainfo, ndraws = 50) {
   summary[["expected_infections"]] <- get_summary_1d_date(
     fit, "iota",
     T_shift = T_shift_latent, .metainfo = .metainfo,
-    intervals = c(0.5, 0.95)
+    var_forecast = "iota_forecast",
+    h_forecast = .metainfo$forecast_horizon,
+    intervals = intervals
   )
   summary[["expected_infections"]]$seeding <- FALSE
   summary[["expected_infections"]][1:(data$G * 2), "seeding"] <- TRUE
@@ -105,7 +111,9 @@ summarize_fit <- function(fit, data, .metainfo, ndraws = 50) {
     summary[["infections"]] <- get_summary_1d_date(
       fit, "I",
       T_shift = T_shift_latent, .metainfo = .metainfo,
-      intervals = c(0.5, 0.95)
+      var_forecast = "I_forecast",
+      h_forecast = .metainfo$forecast_horizon,
+      intervals = intervals
     )
     summary[["infections"]]$seeding <- FALSE
     summary[["infections"]][1:(data$G * 2), "seeding"] <- TRUE
@@ -129,26 +137,32 @@ summarize_fit <- function(fit, data, .metainfo, ndraws = 50) {
   summary[["expected_load"]] <- get_summary_1d_date_log(
     fit, "pi_log",
     T_shift = T_shift_load, .metainfo = .metainfo,
-    intervals = c(0.5, 0.95)
+    var_forecast = "pi_log_forecast",
+    h_forecast = .metainfo$forecast_horizon,
+    intervals = intervalses
   )
 
   summary[["expected_concentration"]] <- get_summary_1d_date_log(
     fit, "kappa_log",
     T_shift = T_shift_load, .metainfo = .metainfo,
-    intervals = c(0.5, 0.95)
+    var_forecast = "kappa_log_forecast",
+    h_forecast = .metainfo$forecast_horizon,
+    intervals = intervals
   )
 
   summary[["concentration"]] <- get_summary_1d_date(
     fit, "predicted_concentration",
     T_shift = T_shift_load, .metainfo = .metainfo,
-    intervals = c(0.5, 0.95)
+    var_forecast = "predicted_concentration_forecast",
+    h_forecast = .metainfo$forecast_horizon,
+    intervals = intervals
   )
 
   if (data$K > 0) {
     # here we exponentiate to get the multiplicative effect
     summary[["sample_effects"]] <- get_summary_vector_log(
       fit, "eta", colnames(data$X),
-      intervals = c(0.5, 0.95)
+      intervals = intervals
     )
   }
 
