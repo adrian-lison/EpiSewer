7abd7d6eaf8aa944539c3f1b6dca5c98c4eb0edb
Adrian Lison <adrian.lison@bsse.ethz.ch>
2024-09-12 14:26:18 +0200

    Update growth report description

--- R/sewer_plot.R
+++ R/sewer_plot.R
@@ -1288,9 +1288,9 @@ plot_prior_posterior <- function(result, param_name) {
 #' Plot a growth report
 #'
 #' @description For a given date, the growth report shows the probability that
-#'   infections have been growing for at least 3, 7, 14, 21, and 28 days,
-#'   respectively. The report uses a diverging bar plot which is scaled between
-#'   "very unlikely" (0% posterior probability) and "very likely" (100%
+#'   infections have been growing constantly for at least 3, 7, 14, 21, and 28
+#'   days, respectively. The report uses a diverging bar plot which is scaled
+#'   between "very unlikely" (0% posterior probability) and "very likely" (100%
 #'   posterior probability).
 #'
 #' @param result Results object returned by [EpiSewer()] after model fitting. In
@@ -1299,7 +1299,10 @@ plot_prior_posterior <- function(result, param_name) {
 #'   fit.
 #' @param date The date for which the growth report should be plotted. If NULL,
 #'   the most recent date for which a reliable report can be provided is
-#'   automatically selected, see `partial_prob`.
+#'   automatically selected, see `partial_prob`. Note that the date is backward
+#'   looking. For example, if `date="2022-03-01"`, you will get the probability
+#'   that have been growing in the last 3, 7, 14, 21, and 28 days before this
+#'   date.
 #' @param partial_prob To select the most recent reliable date, we subtract a
 #'   certain quantile of the shedding load distribution from the current date.
 #'   For example, if `partial_prob=0.8` (default), we select the date for which
@@ -1307,9 +1310,9 @@ plot_prior_posterior <- function(result, param_name) {
 #'   shed.
 #'
 #' @return A growth report plot showing the probability that infections have
-#'   been growing for at least 3, 7, 14, 21, and 28 days, respectively. Can be
-#'   further manipulated using [ggplot2] functions to adjust themes and scales,
-#'   and to add further geoms.
+#'   been growing without interruption for at least 3, 7, 14, 21, and 28 days,
+#'   respectively. Can be further manipulated using [ggplot2] functions to
+#'   adjust themes and scales, and to add further geoms.
 #' @export
 plot_growth_report <- function(result, date = NULL, partial_prob = 0.8) {
   if (!(class(result) == "list" && "summary" %in% names(result))) {
@@ -1356,7 +1359,7 @@ plot_growth_report <- function(result, date = NULL, partial_prob = 0.8) {
       axis.text.x = element_text(face = "bold"),
       ) +
     coord_cartesian(ylim = c(-0.5,0.5)) +
-    ggtitle(paste(
-      "Probability that until", format(as.Date(date_select), "%b %d, %Y"),
-      "infections have been growing for at least..."))
+    ggtitle(paste0(
+      "By ", format(as.Date(date_select), "%b %d, %Y"),
+      ", infections have been growing for the last..."))
 }
