f4809396a3452b301a5c019c8d00e1f898910515
Adrian Lison <adrian.lison@bsse.ethz.ch>
2026-03-16 17:08:20 +0100

    Add jupyter notebook version of README

--- build_README.R
+++ build_README.R
@@ -1,4 +1,16 @@
 allcontributors::add_contributors(format = "text")
 
+# Build markdown file
 rmarkdown::render("README.Rmd", output_format = "github_document")
 unlink("README.html")
+
+# Build jupyter notebook (requires jupytext)
+# You can install it via conda using
+# conda create -n jupytext-env -c conda-forge jupytext
+system2(
+  "/opt/anaconda3/bin/conda",
+  args = c(
+    "run", "-n", "jupytext-env",
+    "jupytext", "--to", "notebook", "README.Rmd"
+    )
+)
