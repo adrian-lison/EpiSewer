24f55988d3f7f6cdaa969e6430c60274b41c2dd3
Adrian Lison <adrian.lison@bsse.ethz.ch>
2026-09-06 10:25:44 +0200

    Explicit FUN arg in frollapply

--- R/sewer_summary.R
+++ R/sewer_summary.R
@@ -87,7 +87,7 @@ summarize_fit <- function(fit, data, .metainfo, intervals = c(0.5, 0.95), ndraws
 
   ## add growth rate
   all_samples[, infectiousness := frollapply(
-      iota, length(gen_dist), weighted.mean,
+      iota, length(gen_dist), FUN = weighted.mean,
       weights = gen_dist
       ), by = ".draw"]
   all_samples[, growth_rate := (
